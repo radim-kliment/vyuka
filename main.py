9a2f5df4ec058b4d781a94c83f473ca74de7a954
radim.kliment <radim.kliment@gmail.com>
2024-11-08 14:26:43 +0100

    2

--- main.py
+++ main.py
@@ -8,12 +8,21 @@ data = pd.read_csv('Sleep_Efficiency.csv')
 
 # ID,Age,Gender,Bedtime,Wakeup time,Sleep duration,Sleep efficiency,REM sleep percentage,Deep sleep percentage,Light sleep percentage,Awakenings,Caffeine consumption,Alcohol consumption,Smoking status,Exercise frequency
 
+# pyenv install 3.11
+# pyenv global 3.11
+# rm -rf env
+# python -m venv env
+# source env/bin/activate
+# pip install pandas keras tensorflow
+
 data = data.drop(['ID', 'Bedtime', 'Wakeup time'], axis=1)
 
 data['Smoking status']  = pd.Categorical(data['Smoking status'])
 data['Smoking status'] = data['Smoking status'].cat.codes
 data['Gender'] = pd.Categorical(data['Gender'])
 data['Gender'] = data['Gender'].cat.codes
+
+data = data.astype('float32')
 
 x = data.drop('Sleep efficiency', axis=1)
 y = data['Sleep efficiency']
@@ -30,5 +39,12 @@ model = keras.Sequential(
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x, y, epochs=50)
 
-print(data.head()) 
+score = model.evaluate(x, y)
+
+if score < 0.2:
+    print("Model is good")
+    converter = tf.lite.TFLiteConverter.from_keras_model(model) 
+    tflite_model = converter.convert()
+    with open('converted_model.tflite', 'wb') as f:     
+        f.write(tflite_model)
 
