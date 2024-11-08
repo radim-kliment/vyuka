b99b2690afc4c118d46825918497ee86a21ba6a4
radim.kliment <radim.kliment@gmail.com>
2024-11-08 13:36:24 +0100

    2

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ from keras import layers
 
 # Load the data
 data = pd.read_csv('Sleep_Efficiency.csv')
+
+# ID,Age,Gender,Bedtime,Wakeup time,Sleep duration,Sleep efficiency,REM sleep percentage,Deep sleep percentage,Light sleep percentage,Awakenings,Caffeine consumption,Alcohol consumption,Smoking status,Exercise frequency
+
+data = data.drop(['ID', 'Bedtime', 'Wakeup time'], axis=1)
+
+print(data.head()) 
+
